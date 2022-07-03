ec26481640385891b5e8a9b6f9c61c3bb982b97c
alina1412 <alina1412@mail.ru>
2022-07-03 12:43:54 +0300

    edit test

--- test_api.py
+++ test_api.py
@@ -1,21 +1,24 @@
 import os
 import sys
+import httpx
 import requests
+import pytest
+
 
 address = ("C:\\Users\\Алина\\AppData\\Local\\" +
            "GitHubDesktop\\git_projects\\morning_bot")
 
 sys.path.append(address)
 import config
-# import tg_chats
+
 from fetchers import site_openweathermap
 dest_with_name = os.path.join(config.Config.TMP_DIR, "randname")
 
-config.Config.PIC_KEY
+
 print(dest_with_name)
 
 
-def get_data():
+def test_get_weather_data():
     url = ("http://api.openweathermap.org" +
            f"/data/2.5/weather?q={site_openweathermap.Openweather.city}")
     params = {"units": "metric",
@@ -24,4 +27,16 @@ def get_data():
     assert resp.ok is True
 
 
-get_data()
+# pixabay is banned
+def test_get_pic_data():
+    url = 'https://pixabay.com/api/'
+    params = {"key": config.Config.PIC_KEY, "q": "morning",
+                "safesearch": "true", "per_page": 20}
+    resp = requests.get(url, params=params)
+  
+    assert resp.ok is True
+    data = resp.json()
+    # print()
+
+# test_get_data()
+# test_get_pic_data()
